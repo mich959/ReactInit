9bdc1832a41684a304773c3f1cbebc9d3d21bf6a
mich959 <42066332+mich959@users.noreply.github.com>
2018-08-09 10:47:25 +0200

    s4l56

--- src/App.js
+++ src/App.js
@@ -21,15 +21,12 @@ class App extends Component {
     });
   };
 
-  switchNameHandler = newName => {
-    this.setState({
-      persons: [
-        { name: newName, age: 31 },
-        { name: "Gilles", age: 1 },
-        { name: "Anneleen", age: 27 }
-      ]
-    });
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
@@ -42,22 +39,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Mich!")}
-            changed={this.nameChangedHandler}
-          >
-            Child of Mich Anneleen
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
+          })}
         </div>
       );
     }
